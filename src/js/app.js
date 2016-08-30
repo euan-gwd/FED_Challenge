c8ddccb56c8b7f2c4eeee34a06897392cb7fbd75
Euan_G <euan@amwic.co.za>
2016-08-30 02:43:00 +0200

    Completed Challenge 2

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,14 @@
 
 	$(function() {
 		getWeather();
+		randomize();
 	});
 
 	function getWeather() {
-		const weatherAPIUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-		// const weatherAPI = 'js/yql.json';
-		// $.get(weatherAPI, successFn);
-		$.get(weatherAPIUrl, successFn);
+		// const weatherAPIUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+		// $.get(weatherAPIUrl, successFn);
+		const weatherAPI = 'js/yql.json';
+		$.get(weatherAPI, successFn);
 	}
 
 	function successFn(data) {
@@ -24,6 +25,21 @@
 																													<li>Low:\t${forecast.low}</li>
 																													<li>${forecast.text}</li>
 																													</ul>`);
+		});
+	}
+
+	function randomize() {
+		const drawItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+		let counter = 0;
+		while (counter < 7) {
+			let winner = Math.floor(Math.random() * counter);
+			counter++;
+			let shuffle = drawItems[counter];
+			drawItems[counter] = drawItems[winner];
+			drawItems[winner] = shuffle;
+		}
+		$.each(drawItems, function(i, drawItem) {
+			$('.random-number-app').append(`<p class="prize">${drawItem}</p>`);
 		});
 	}
 
